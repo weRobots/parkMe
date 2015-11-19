3ee51a12ce6e658023f28b291b1b10690c795f42
unknown <supun.hettigoda@CWLK-SUPUNH.cambio.se>
2015-11-19 14:47:16 +0530

    added the basic web-server structure with jersey oriented RESTful service classes. Mssql DB is needed. application server will be added later.

---
+++ web-server/src/com/prgguru/jersey/Utility.java
@@ -0,0 +1,63 @@
+package com.prgguru.jersey;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+public class Utility {
+	/**
+	 * Null check Method.
+	 *
+	 * @param txt
+	 *
+	 * @return
+	 */
+	public static boolean isNotNull(final String txt) {
+		// System.out.println("Inside isNotNull");
+		return ((txt != null) && (txt.trim().length() >= 0)) ? true : false;
+	}
+
+	/**
+	 * Method to construct JSON.
+	 *
+	 * @param tag
+	 * @param status
+	 *
+	 * @return
+	 */
+	public static String constructJSON(final String tag, final boolean status) {
+		final JSONObject obj = new JSONObject();
+
+		try {
+			obj.put("tag", tag);
+			obj.put("status", new Boolean(status));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+		}
+
+		return obj.toString();
+	}
+
+	/**
+	 * Method to construct JSON with Error Msg.
+	 *
+	 * @param tag
+	 * @param status
+	 * @param err_msg
+	 *
+	 * @return
+	 */
+	public static String constructJSON(final String tag, final boolean status,
+			final String err_msg) {
+		final JSONObject obj = new JSONObject();
+
+		try {
+			obj.put("tag", tag);
+			obj.put("status", new Boolean(status));
+			obj.put("error_msg", err_msg);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+		}
+
+		return obj.toString();
+	}
+}
