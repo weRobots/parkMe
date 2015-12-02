1f3f2477f58099df7580e18f4e8dff7a57f8db4b
unknown <supun.hettigoda@CWLK-SUPUNH.cambio.se>
2015-12-02 18:43:11 +0530

    small improvements

--- client/app/src/main/java/com/robots/we/parkme/network/NetworkConfigurations.java
+++ client/app/src/main/java/com/robots/we/parkme/network/NetworkConfigurations.java
@@ -6,5 +6,8 @@ package com.robots.we.parkme.network;
  * Created by supun.hettigoda on 11/28/2015.
  */
 public class NetworkConfigurations {
-    public static String URL_REFRESH = "http://10.0.2.2:6060/parkme/refresh/dorefresh?id=01";
+    private static String HOST_WIFY = "http://172.20.61.22:6060/";
+    private static String HOST_EMULATOR = "http://10.0.0.2:6060/";
+    private static String REFRESH_PATH = "parkme/refresh/dorefresh?id=01";
+    public static String URL_REFRESH = HOST_WIFY + REFRESH_PATH;
 }
